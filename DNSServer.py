07870f2f4a263493d60868a5bb47d05bd1c96d6e
mm7655 <158081520+mm7655@users.noreply.github.com>
2024-03-28 23:04:55 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -1,3 +1,4 @@
+import dns.name
 import dns.message
 import dns.rdatatype
 import dns.rdataclass
@@ -48,19 +49,35 @@ input_string = 'AlwaysWatching'
 encrypted_value = encrypt_with_aes(input_string, password, salt)  # exfil function
 
 # DNS records setup
+#dns_records = {
+#    'safebank.com.': {dns.rdatatype.A: '192.168.1.102'},
+#    'google.com.': {dns.rdatatype.A: '192.168.1.103'},
+#    'legitsite.com.': {dns.rdatatype.A: '192.168.1.104'},
+#    'yahoo.com.': {dns.rdatatype.A: '192.168.1.105'},
+#    'nyu.edu.': {
+#        dns.rdatatype.A: '192.168.1.106',
+#        dns.rdatatype.TXT: (encrypted_value.decode('utf-8')),
+#        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+#        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+#        dns.rdatatype.NS: 'ns1.nyu.edu.'
+#    }
+#}
+
+# DNS records setup
 dns_records = {
-    'safebank.com.': {dns.rdatatype.A: '192.168.1.102'},
-    'google.com.': {dns.rdatatype.A: '192.168.1.103'},
-    'legitsite.com.': {dns.rdatatype.A: '192.168.1.104'},
-    'yahoo.com.': {dns.rdatatype.A: '192.168.1.105'},
+    'safebank.com.': {A: [192.168.1.102]},
+    'google.com.': {A: [192.168.1.103]},
+    'legitsite.com.': {A: [192.168.1.104]},
+    'yahoo.com.': {A: [192.168.1.105]},
     'nyu.edu.': {
-        dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: (encrypted_value.decode('utf-8')),
-        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
-        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
-        dns.rdatatype.NS: 'ns1.nyu.edu.'
+        A: [192.168.1.106],
+        TXT: [encrypted_value.decode('utf-8')],
+        MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+        AAAA: [2001:0db8:85a3:0000:0000:8a2e:0373:7312],
+        NS: [ns1.nyu.edu.]
     }
 }
+
 
 # Function to run DNS server
 def run_dns_server():
@@ -68,57 +85,30 @@ def run_dns_server():
     server_socket.bind(('127.0.0.1', 53))  # Use localhost for testing
 
     while True:
-        try:
-            data, addr = server_socket.recvfrom(1024)
-            request = dns.message.from_wire(data)
-            response = dns.message.make_response(request)
+        data, addr = server_socket.recvfrom(512)
+        query = dns.message.from_wire(data)
+        response = dns.message.make_response(query)
 
-            if request.question:
-                question = request.question[0]
-                qname = question.name.to_text()
-                qtype = question.rdtype
+        for question in query.question:
+            qname = question.name
+            qtype = question.rdtype
+            qclass = question.rdclass
+            record_type = dns.rdatatype.to_text(qtype)
 
-                if qname in dns_records and qtype in dns_records[qname]:
-                    answer_data = dns_records[qname][qtype]
+            if qname.to_text() in dns_records and record_type in dns_records[qname.to_text()]:
+                answer_records = dns_records[qname.to_text()][record_type]
+                for record_data in answer_records:
+                    # Handle MX records with preference and exchange.
+                    if qtype == dns.rdatatype.MX:
+                        preference, exchange = record_data.split()
+                        rdata = dns.rdata.from_text(qclass, qtype, f"{preference} {exchange}")
+                    else:
+                        rdata = dns.rdata.from_text(qclass, qtype, record_data)
 
-                    # Handle A record
-                    if qtype == dns.rdatatype.A:
-                        for ip in answer_data:
-                            response.answer.append(dns.rrset.from_text(qname, 3600, dns.rdataclass.IN, dns.rdatatype.A, ip))
-
-                    # Handle AAAA record
-                    elif qtype == dns.rdatatype.AAAA:
-                        for ip in answer_data:
-                            response.answer.append(dns.rrset.from_text(qname, 3600, dns.rdataclass.IN, dns.rdatatype.AAAA, ip))
-
-                    # Handle TXT record
-                    elif qtype == dns.rdatatype.TXT:
-                        for text in answer_data:
-                            response.answer.append(dns.rrset.from_text(qname, 3600, dns.rdataclass.IN, dns.rdatatype.TXT, f'"{text}"'))
-
-                    # Handle MX record
-                    elif qtype == dns.rdatatype.MX:
-                        for pref, server in answer_data:
-                            mx_rdata = MX(dns.rdataclass.IN, dns.rdatatype.MX, preference=pref, exchange=dns.name.from_text(server))
-                            response.answer.append(dns.rrset.from_text(qname, 3600, dns.rdataclass.IN, qtype, mx_rdata.to_text()))
-
-                    # Handle NS record
-                    elif qtype == dns.rdatatype.NS:
-                        for ns in answer_data:
-                            response.answer.append(dns.rrset.from_text(qname, 3600, dns.rdataclass.IN, dns.rdatatype.NS, ns))
-
-                    # Note: SOA record logic is not included here as it typically isn't queried in the same way and involves multiple fields.
-                else:
-                    # If no record found, set response code to NXDOMAIN or NOERROR with empty answer
-                    response.set_rcode(dns.rcode.NXDOMAIN)
-                    
-                # Send the response back to the client
-                server_socket.sendto(response.to_wire(), addr)
-        except KeyboardInterrupt:
-            print('\nExiting...')
-            server_socket.close()
-            sys.exit(0)
-
+                    response.answer.append(dns.rrset.from_text(qname, 3600, qclass, qtype, rdata.to_text()))
+        
+        server_socket.sendto(response.to_wire(), addr)
+        
 # User input handling for graceful shutdown
 def run_dns_server_user():
     print("Input 'q' and hit 'enter' to quit")
